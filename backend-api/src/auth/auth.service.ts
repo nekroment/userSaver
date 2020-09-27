d47a55cb731723e4c2aca3f6f72c810cacd70b84
nekroment <adamcevs@gmail.com>
2020-09-27 11:22:28 +0300

    add login methods

--- backend-api/src/auth/auth.service.ts
+++ backend-api/src/auth/auth.service.ts
@@ -1,4 +1,44 @@
+import { User } from './../schema/User';
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { google } from 'googleapis';
+require('dotenv/config');
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+    async findUser(token: string): Promise<User> {
+        return this.userModel.findOne({token: token});
+    }
+
+    async saveUser(name: string, email: string, img: string, token: string): Promise<User> {
+        const newUser = new this.userModel({
+            name,
+            email,
+            img,
+            token
+        });
+        return newUser.save();
+    }
+
+    async createConnectionString(redirect: string, code: string): Promise<{}> {
+        const googleConfig = {
+            clientId: `${process.env.CLIENT_ID}`, 
+            clientSecret: `${process.env.CLIENT_SECRET}`, 
+            redirect 
+          };
+
+        const auth = new google.auth.OAuth2(
+            googleConfig.clientId,
+            googleConfig.clientSecret,
+            googleConfig.redirect
+        );
+
+        const data = await auth.getToken(code);
+        return data.tokens.id_token;
+
+
+    }
+}
