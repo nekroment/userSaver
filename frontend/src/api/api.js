956c6ffab0bcf49ffb9f19e13b7e4b78cea087d1
nekroment <adamcevs@gmail.com>
2020-09-27 12:40:25 +0300

    add login and saveUser requests

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,4 +2,28 @@ import * as axios from 'axios'
 
 const instance = axios.create({
     baseURL: 'http://localhost:3000'
-})
+});
+
+export const authAPI = {
+    async connection(redirect) {
+        return await instance.post('/auth/connection', {redirect});
+    },
+    async login(token, redirect, code) {
+        return await instance.post('/auth/login', {token, redirect, code});
+    }
+}
+
+export const userAPI = {
+    async getUsers() {
+        return await axios.get('https://jsonplaceholder.typicode.com/users');
+    },
+    async findeUsers(id) {
+        return await instance.get(`/user/${id}`);
+    },
+    async deleteUser(id) {
+        return await instance.delete(`/user/${id}`);
+    },
+    async saveUser(user, id) {
+        return await instance.post('/user', {user, id});
+    }
+}
