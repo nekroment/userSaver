4d2d614decba1698acd446bd50d1386cf5c98e40
nekroment <66136328+nekroment@users.noreply.github.com>
2020-09-30 11:30:24 +0300

    Update app.module.ts
    change connectString

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -4,7 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 require('dotenv/config');
 
-const connectionString = 'mongodb+srv://moonlight:382001@cluster0.zkmbj.mongodb.net/saveUser?retryWrites=true&w=majority'
+const connectionString = `mongodb+srv:/${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0.zkmbj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
 @Module({
   imports: [MongooseModule.forRoot(connectionString), AuthModule, UserModule],
   controllers: [],
